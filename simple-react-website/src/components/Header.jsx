c5a3a829defc0f20d5acf8e08b422a2e9cfaa3a5
Landon Zigler <101595765+Landoidd@users.noreply.github.com>
2024-08-29 10:46:38 -0400

    Initialized Header Component

---
+++ simple-react-website/src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+
+    return(
+        <header>
+            <h1>Simple React Website</h1>
+            <nav>
+                <ui>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                </ui>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
